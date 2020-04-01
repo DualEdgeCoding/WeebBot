fdbd79dec06cb0b7a1bc5dbcbfe4ed1848780682
weebkun <2002umpa@gmail.com>
2020-04-01 22:06:37 +0800

    finished UserJoin listener.

--- src/main/java/com/weebkun/Application.java
+++ src/main/java/com/weebkun/Application.java
@@ -2,6 +2,7 @@ package com.weebkun;
 
 import com.weebkun.commands.Hungry;
 import com.weebkun.events.ExampleListener;
+import com.weebkun.events.UserJoined;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
@@ -17,8 +18,12 @@ public class Application {
 
         JDA jda = JDABuilder.createDefault(BOT_TOKEN).build();
 
+        //listeners for commands
         jda.addEventListener(new ExampleListener());
         jda.addEventListener(new Hungry());
+
+        //listeners for events
+        jda.addEventListener(new UserJoined());
 
     }
 }
--- src/main/java/com/weebkun/events/UserJoined.java
+++ src/main/java/com/weebkun/events/UserJoined.java
@@ -4,6 +4,8 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Random;
+
 /**
  * listener for GuildJoinEvent.
  * sends welcome message to user that just joined.
@@ -11,11 +13,29 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  */
 public class UserJoined extends ListenerAdapter {
 
+    private static final String[] MESSAGES = {
+            "[member] joined. You must construct additional pylons.",
+            "Never gonna give [member] up. Never let [member] down!",
+            "Hey! Listen! [member] has joined!",
+            "Ha! [member] has joined! You activated my trap card!",
+            "We've been expecting you, [member].",
+            "It's dangerous to go alone, take [member]!",
+            "Swoooosh. [member] just landed.",
+            "Brace yourselves. [member] just joined the server.",
+            "A wild [member] appeared.",
+            "Hi [member] i'm dad.",
+            "Activate operation [member]-has-joined-the-server!"
+    };
+
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent e) {
+        //instantiates new EmbedBuilder for embed message
         EmbedBuilder builder = new EmbedBuilder();
+        //gets random index for messages list
+        Random r = new Random();
+        int num = r.nextInt(MESSAGES.length);
 
-        builder.setColor(65280).setThumbnail(e.getUser().getAvatarUrl()).setTitle("Welcome!").setDescription(e.getUser().getName());
+        builder.setColor(65280).setThumbnail(e.getUser().getAvatarUrl()).setTitle("Welcome!").setDescription(e.getUser().getName()).setFooter(MESSAGES[num].replaceAll("[member]", e.getUser().getAsMention()));
         e.getGuild().getDefaultChannel().sendMessage(builder.build()).queue();
     }
 }
